13b5b08b1f0b5f5a39078b15040b5dedad972dbc
naufal67741 <46347094+naufal67741@users.noreply.github.com>
2021-09-01 13:22:07 +0700

    again

--- NETCore/NETCore/Repository/Data/PersonRepository.cs
+++ NETCore/NETCore/Repository/Data/PersonRepository.cs
@@ -152,13 +152,13 @@ namespace NETCore.Repository.Data
             return false;
         }
         //test
-        public int Login(PersonVM personVM)
+        public int Login(LoginVM loginVM)
         {
             //return 100 = email ga ketemu
             //return 200 = password salah
             //return 1 = login berhasil
-            var checkEmail = myContext.Persons.Where(e => e.Email == personVM.Email).FirstOrDefault();
-            if(checkEmail == null)
+            var checkEmail = myContext.Persons.Where(e => e.Email == loginVM.Email).FirstOrDefault();
+            if (checkEmail == null)
             {
                 return 100;
             }
@@ -168,7 +168,7 @@ namespace NETCore.Repository.Data
             {
                 return 100;
             }
-            if (account.Password != personVM.Password)
+            if (account.Password != loginVM.Password)
             {
                 return 200; //Nambah Comment ini
             }
